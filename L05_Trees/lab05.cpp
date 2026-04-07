b04bb450b6be994beafc5b80305545eca44f70f7
Highlander2323 <rauloctav2001@gmail.com>
2026-04-07 07:33:18 +0300

    Sum updates

--- L05_Trees/lab05.cpp
+++ L05_Trees/lab05.cpp
@@ -15,6 +15,18 @@ struct NodArbore {
     }
 };
 
+struct Fisier {
+    char nume[100];
+    Fisier* primulFiu;
+    Fisier* urmatorulFrate;
+
+    void initializare(char nume[100]){
+        primulFiu = nullptr;
+        urmatorulFrate = nullptr;
+        strcpy(this->nume, nume);
+    }
+};
+
 // Adaugă un copil unui părinte
 void adaugaCopil(NodArbore* parinte, int val) {
     NodArbore* nou = new NodArbore(val);
@@ -98,6 +110,49 @@ int calculeazaAdancime(NodArbore* nod){
     return max(gradCopil, gradFrate);
 }
 
+void adaugaFolder(Fisier *root, Fisier* nou){;
+    if(root->primulFiu == nullptr){
+        root->primulFiu = nou;
+    } else {
+        Fisier* temp = root->primulFiu;
+        while(temp->urmatorulFrate != nullptr){
+            temp = temp->urmatorulFrate;
+        }
+        temp->urmatorulFrate = nou;
+    }
+}
+
+void adaugaFisier(Fisier* parinte, Fisier* nou){
+    if(parinte->primulFiu == nullptr){
+        parinte->primulFiu = nou;
+    } else {
+        Fisier* temp = parinte->primulFiu;
+        while(temp->urmatorulFrate != nullptr){
+            temp = temp->urmatorulFrate;
+        }
+        temp->urmatorulFrate = nou;
+    }
+}
+
+void afiseazaFisiere(Fisier* nod, int nivel){
+    if(nod == nullptr){
+        return;
+    }
+
+    int count = nivel;
+
+    while(count > 0){
+        if(count == 1){
+            cout << "|_ ";    
+        }else{
+            cout << "\t";
+        }
+        count--;
+    }
+    cout << nod->nume << endl;
+    afiseazaFisiere(nod->primulFiu, nivel + 1);
+    afiseazaFisiere(nod->urmatorulFrate, nivel);
+}
 
 int main() {
     NodArbore* radacina = new NodArbore(0);
@@ -123,6 +178,61 @@ int main() {
     cout << "\nGrad maxim arbore: " << gradMaximArbore(radacina);
 
     cout << "\nAdancime arbore: " << calculeazaAdancime(radacina);
+    
+    cout << endl;
 
+    Fisier* sistemFisiere = new Fisier();
+    sistemFisiere->initializare("C:");
+    bool run = true;
+    while(run){
+        int opt;
+        cout << "1. Adauga folder\n";
+        cout << "2. Adauga fisier\n";
+        cout << "3. Afișează structura\n";
+        cout << "Optiune: ";
+        cin >> opt;
+        cin.ignore();
+        switch(opt){
+            case 1: {
+                char nume[100];
+                cout << "Nume folder: ";
+                cin.getline(nume, 100);
+                Fisier* nou = new Fisier();
+                nou->initializare(nume);
+                adaugaFolder(sistemFisiere, nou);
+                break;
+            }
+            case 2: {
+                char nume[100];
+                Fisier* parinte;
+                int folder;
+                cout << "Nume fisier: ";
+                cin.getline(nume, 100);
+                Fisier* nou = new Fisier();
+                nou->initializare(nume);
+
+                cout << "Unde? : ";
+                Fisier *temp = sistemFisiere->primulFiu;
+                int count = 1;
+                Fisier* foldere[100];
+                while(temp != nullptr){
+                    cout << count << ". " << temp->nume << " ";
+                    foldere[count++] = temp;
+                    temp = temp->urmatorulFrate;
+                }
+                cin >> folder;
+                cin.ignore();
+
+                adaugaFisier(foldere[folder], nou);
+                break;
+            }
+            case 3: {
+                afiseazaFisiere(sistemFisiere, 0);
+                break;
+            }
+            default:
+                run = false;
+        }
+    }
     return 0;
 }
